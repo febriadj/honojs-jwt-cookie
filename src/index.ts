7fb86bf532ef37ce5c228c6c2485774d674b05ea
febriadj <iamfebriadji@gmail.com>
2023-04-30 12:44:06 +0700

    feat(api): add login route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
+import { sign } from 'hono/utils/jwt/jwt';
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcryptjs';
 import setError from './helpers/setError';
 
 export type IEnv = {
   D1: D1Database;
+  JWT_SECRET: string;
 };
 
 const app = new Hono<{ Bindings: IEnv }>();
@@ -42,7 +44,41 @@ app.post('/register', async (c) => {
     });
 
     c.status(error.code);
+    return c.json(error);
+  }
+});
 
+app.post('/login', async (c) => {
+  try {
+    const { email, password } = await c.req.json();
+
+    const stmt: { id: string; email: string; password: string } =
+      await c.env.D1.prepare(
+        'SELECT id, email, password FROM users WHERE email = ?'
+      )
+        .bind(email)
+        .first();
+
+    if (!stmt || !(await bcrypt.compare(password, stmt.password))) {
+      throw new Error(!stmt ? 'User does not exist' : 'Invalid password');
+    }
+
+    // Generate JWT token
+    const token = await sign({ id: stmt.id }, c.env.JWT_SECRET);
+
+    // Store JWT token in cookies
+    c.cookie('accessToken', token, { httpOnly: true });
+
+    return c.json({ success: true });
+  } catch (err) {
+    const error = setError((obj) => {
+      if (err instanceof Error) {
+        obj['message'] = err.message;
+        obj['code'] = 401;
+      }
+    });
+
+    c.status(error.code);
     return c.json(error);
   }
 });
